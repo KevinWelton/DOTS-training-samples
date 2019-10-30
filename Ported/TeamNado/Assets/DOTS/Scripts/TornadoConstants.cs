d731c0a1f0648e0da74a19677e877bb3a552fdfa
Kevin Welton <kevinwe@unity3d.com>
2019-10-30 16:08:09 -0700

    Split out tornado constants into a separate static class so they can be used by multiple systems. It's sloppy. But it's what we got.

---
+++ Ported/TeamNado/Assets/DOTS/Scripts/TornadoConstants.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TornadoConstants
+{
+    private const float TornadoMaxForceDistance = 30.0f;
+    private const float UpForce = 1.4f;
+    private const float InwardForce = 9.0f;
+    private const float TornadoHeight = 50.0f;
+    private const float Friction = 0.4f;
+    private const float Damping = 0.012f;
+    private const float MaxForce = 30.0f;
+    private const float Gravity = 1.0f;
+}
