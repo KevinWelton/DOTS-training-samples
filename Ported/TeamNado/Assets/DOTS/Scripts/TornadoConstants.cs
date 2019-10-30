936b229274a7dc222e9be50e7b53335c020ead92
Kevin Welton <kevinwe@unity3d.com>
2019-10-30 16:09:26 -0700

    Make TornadoConstant members public so we can actually use them

--- Ported/TeamNado/Assets/DOTS/Scripts/TornadoConstants.cs
+++ Ported/TeamNado/Assets/DOTS/Scripts/TornadoConstants.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class TornadoConstants
+internal static class TornadoConstants
 {
-    private const float TornadoMaxForceDistance = 30.0f;
-    private const float UpForce = 1.4f;
-    private const float InwardForce = 9.0f;
-    private const float TornadoHeight = 50.0f;
-    private const float Friction = 0.4f;
-    private const float Damping = 0.012f;
-    private const float MaxForce = 30.0f;
-    private const float Gravity = 1.0f;
+    public const float TornadoMaxForceDistance = 30.0f;
+    public const float UpForce = 1.4f;
+    public const float InwardForce = 9.0f;
+    public const float TornadoHeight = 50.0f;
+    public const float Friction = 0.4f;
+    public const float Damping = 0.012f;
+    public const float MaxForce = 30.0f;
+    public const float Gravity = 1.0f;
 }
