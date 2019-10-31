1b05a85d252176c090316deea637e5b0162bcee1
Erik Sooter <erikso@unity3d.com>
2019-10-31 13:59:21 -0700

    some number stuff

--- Ported/TeamNado/Assets/DOTS/Scripts/TornadoConstants.cs
+++ Ported/TeamNado/Assets/DOTS/Scripts/TornadoConstants.cs
@@ -6,7 +6,7 @@ internal static class TornadoConstants
 {
     public const float TornadoMaxForceDistance = 30.0f;
     public const float UpForce = 1.3f;
-    public const float InwardForce = 9.0f;
+    public const float InwardForce = 40.0f;
     public const float TornadoHeight = 50.0f;
     public const float Friction = 0.4f;
     public const float Damping = 0.012f;
